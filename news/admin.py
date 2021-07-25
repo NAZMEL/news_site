5a383b6b9400d2b18186f6de8033d1df92f92bbe
NAZMEL <melnyknasar@gmail.com>
2021-07-25 23:21:03 +0300

    formatting django.admin

--- news/admin.py
+++ news/admin.py
@@ -1,4 +1,28 @@
 from django.contrib import admin
+from .models import News
 
 
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    # it shows values we want to see in django.admin
+    list_display = (
+        'id',
+        'title',
+        'content',
+        'created_at',
+        'updated_at',
+        'is_published'
+    )
 
+    # it shows values with links we want to use in django.admin
+    list_display_links = (
+        'id',
+        'title'
+    )
+
+    # create field for searching
+    # tuple of column in which we want to look for
+    search_fields = (
+        'title',
+        'content'
+    )
--- news/models.py
+++ news/models.py
@@ -2,13 +2,18 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150, verbose_name = 'Name of article')
     content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%y/%m/%d/')
-    is_published = models.BooleanField(default=True)                      # is_published = 1
-
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Publish date')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Update date')
+    photo = models.ImageField(upload_to='photos/%y/%m/%d/', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Is published?')                      # is_published = 1
 
     def __str__(self):
         return self.title
+
+    # class for django.admin
+    class Meta:
+        verbose_name = 'Новина'         # singular name of table
+        verbose_name_plural = 'Новини'   # plural name
+        ordering = ['-created_at']
