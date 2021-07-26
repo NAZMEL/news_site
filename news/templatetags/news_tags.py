c24db7f3145b2275c8a3f2524b416cd291dd319e
NAZMEL <melnyknasar@gmail.com>
2021-07-26 16:27:19 +0300

    added tags

---
+++ news/templatetags/news_tags.py
@@ -0,0 +1,20 @@
+from django import template
+from news.models import Category
+
+register = template.Library()
+
+
+@register.simple_tag(name = 'get_list_categories')
+def get_categories():
+    return Category.objects.all()
+
+
+@register.inclusion_tag('news/list_categories.html')
+def show_categories():
+    categories = Category.objects.all()
+    
+    context = {
+        'cattegories': categories
+    }
+
+    return context
--- news/views.py
+++ news/views.py
@@ -9,12 +9,10 @@ def index(request):
     Get all news starting with the latest one
     '''
     news = News.objects.order_by("-created_at")
-    categories = Category.objects.all()
 
     context = {
         'news': news, 
         'title': 'News list',
-        'categories': categories
         }
 
     return render(request, 'news/index.html', context = context)
@@ -25,12 +23,10 @@ def get_category(request, category_id):
     Gat news for for specific category
     '''
     news = News.objects.filter(category_id = category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk = category_id)
 
     context = {
         'news': news,
-        'categories': categories,
         'category': category
     }
 
